51e2c6d5ed0cf731850d713ffa09d5f1750923c5
poojankhanpara <poojankhanpara@gmail.com>
2024-06-16 13:14:37 -0700

    added feature to add item to the list

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from typing import Union, List
-from fastapi import FastAPI, HTTPException
-from ListModel import ListModel, InMemDb, NewListModel
+from fastapi import FastAPI, HTTPException, Response
+from ListModel import ListModel, InMemDb, NewListModel, ListItemModel
 from ListDB import InMem, InMemList, ListItem
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+# API doc is here http://127.0.0.1:8000/docs#/
 # to enable CORS
 origins = ["http://localhost:3000"]
 app.add_middleware(CORSMiddleware, allow_origins=origins)
@@ -27,13 +27,14 @@ inmemdb.lists["hacked list"] = None
 ## priming ends
 
 
+# TODO add api versioning /v1/
 @app.get("/")
 def read_root():
     return {"lists": list(inmemdb.lists.keys())}
 
 
 @app.get("/list")
-def read_lists():
+def get_all_lists():
     return {"lists": list(inmemdb.lists.keys())}
 
 
@@ -48,6 +49,15 @@ def create_list(new_list: NewListModel):
         )  # note this is a Fastapi httpexception
 
 
+@app.delete("/list")
+def remove_list(old_list: NewListModel):
+    if old_list.name not in inmemdb.lists:
+        return "{}"
+    else:
+        del inmemdb.lists[old_list.name]
+        return "{}"
+
+
 @app.get("/list/{list_name}")
 def get_items_from_list(list_name: str):
     if list_name in inmemdb.lists:
@@ -60,11 +70,15 @@ def get_items_from_list(list_name: str):
 
 
 @app.post("/list/{list_name}")
-def add_item_to_list(list_name: str, item_name):
+def add_item_to_list(list_name: str, item: ListItemModel):
     if list_name in inmemdb.lists:
-        print(inmemdb.lists[list_name])
-        # TODO add item to the list
+        inmemdb.lists[list_name].list_items.append(item)
+        return inmemdb.lists[list_name]
     else:
         raise HTTPException(
-            status_code=404, detail=f"{list_name} list does not exists!"
+            status_code=404, detail=f"{list_name} list does not exists to add {item}!"
         )  # note this is a Fastapi httpexception
+
+
+# TODO add PUT/update item to checked
+# TODO remove an item from the list
