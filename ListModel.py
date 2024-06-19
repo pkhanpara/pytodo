f0e855834029c00f1272f890871ecd8210fb248d
Poojan <poojankhanpara@gmail.com>
2024-06-19 16:00:11 -0400

    Update items (#4)
    * added id to item and list so we could update it later on
    
    * feature: update item in a list
    
    * added delete item in a list

--- ListModel.py
+++ ListModel.py
@@ -1,21 +1,25 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from typing import List, Dict
+from uuid import UUID, uuid4
 
 
 # pydantic models are used to validate the user input coming in the form of json
 class ListItemModel(BaseModel):
-    item: str
+    name: str
     checkmark: bool
+    id: UUID = Field(default_factory=uuid4)
 
 
 class ListModel(BaseModel):
     name: str
-    list_items: List[ListItemModel]
+    items: List[ListItemModel]
+    id: UUID
 
 
 class NewListModel(ListModel):
     ListModel.name: str
-    ListModel.list_items = []
+    ListModel.items = []
+    ListModel.id = Field(default_factory=uuid4)
 
 
 class InMemDb(BaseModel):
